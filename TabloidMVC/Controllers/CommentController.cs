81ed915caaa7dca61a2223813ce8728b548c3ae7
AlexLam12 <Alex.e015@gmail.com>
2021-09-27 16:05:32 -0500

    Almost there comments

---
+++ TabloidMVC/Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TabloidMVC.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
+using System.Security.Claims;
+using TabloidMVC.Models.ViewModels;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    [Authorize]
+    public class CommentController : Controller
+    {
+        private readonly IPostRepository _postRepository;
+        private readonly ICommentRepository _commentRepository;
+
+        public CommentController(IPostRepository postRepository, ICommentRepository commentRepository)
+        {
+            _postRepository = postRepository;
+            _commentRepository = commentRepository;
+        }
+        public IActionResult Index()
+        {
+            var comments = _commentRepository.GetPostComments();
+            return View(comments);
+        }
+    }
+}
