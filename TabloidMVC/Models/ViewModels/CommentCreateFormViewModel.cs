938cc53d9591a54bbcfd151675dd4abe17a1ebc6
AlexLam12 <Alex.e015@gmail.com>
2021-09-29 13:18:53 -0500

    create comment is working

---
+++ TabloidMVC/Models/ViewModels/CommentCreateFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TabloidMVC.Models.ViewModels
+{
+    public class CommentCreateFormViewModel
+    {
+        public Comment Comment { get; set; }
+        public Post Post { get; set; }
+    }
+}
